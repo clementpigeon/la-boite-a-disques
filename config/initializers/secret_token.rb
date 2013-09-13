bdc2a92243fa4a31fdf0e6f499e6a3f27a059892
App Academy Student <student@appacademy.io>
2013-09-13 09:17:13 -0400

    Creates Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaBoiteADisques::Application.config.secret_token = '329de103ff18debde8cf4732e1a3279be9981358973420db4e0cdda4038302ea7322e713f925955bdcd9e6ce89330e098d5d23b9f8a8618fad8f15681abaa7c9'
